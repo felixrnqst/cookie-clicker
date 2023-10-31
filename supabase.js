80f8c2cf91183148986993e690617ebd3c7ae2d4
Anopy <guillaume_roboam@hotmail.fr>
2023-10-31 23:54:34 +0100

    Beginning Backend v2

---
+++ supabase.js
@@ -0,0 +1,13 @@
+/*
+supabase.js - Created by Guillaume
+This export the connection client to the DB with supabase to use it in all the files needed
+*/
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+console.log(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
